aa18fb4779d9456ad462f7c221b9cfe56e323824
kamran <mohammadkamranahmad786@gmail.com>
2024-04-20 10:57:49 +0000

    solution

---
+++ src/components/TransactionItem/index.js
@@ -0,0 +1,35 @@
+// Write your code here
+import './index.css'
+
+const TransactionItem = props => {
+  const {TransactionDetails, deleteTransaction} = props
+  const {id, title, amount, type} = TransactionDetails
+
+  const onDeleteTransaction = () => {
+    deleteTransaction(id)
+  }
+
+  return (
+    <li className="table-row">
+      <p className="transaction-text">{title}</p>
+      <p className="transaction-text">Rs {amount}</p>
+      <p className="transaction-text">{type}</p>
+      <div className="delete-container">
+        <button
+          className="delete-button"
+          type="button"
+          onClick={onDeleteTransaction}
+          data-testid="delete"
+        >
+          <img
+            className="delete-img"
+            src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default TransactionItem
